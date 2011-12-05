d29e9db5547226a716edb72c968f6c4fc36bd3e5
Anup Mahabudhe <anupwin@gmail.com>
2011-12-05 15:18:01 +0530

    app controller updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,10 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
       redirect_to root_url, :alert => exception.message
     end
+    
+    def current_ability
+      @current_ability ||= Ability.new(current_user)
+    end
+    
   
 end
