ece97e5b657a8b60b29aa10f2e6800f40b48e74a
Anup Mahabudhe <anupwin@gmail.com>
2011-12-05 11:19:44 +0530

    ability added

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,5 +4,18 @@ class Ability
   def initialize(user)
       user ||= User.new # guest user
 
-     end
+      if user.role? :superadmin
+        can :manage, :all
+      elsif user.role? :administrator
+        can :update, :all
+        can :new, :all
+        can :read, [User]
+        can :manage, [User]
+      elsif user.role? :staff
+        can :read, [User]
+        
+      else
+        can :manage, [Role]
+      end
+    end
   end
