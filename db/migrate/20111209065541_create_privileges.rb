f0cb650b9124779139d0cda8d17e581668fd2e20
Anup Mahabudhe <anupwin@gmail.com>
2011-12-13 14:59:53 +0530

    Priviliege migration updated

--- db/migrate/20111209065541_create_privileges.rb
+++ db/migrate/20111209065541_create_privileges.rb
@@ -5,19 +5,6 @@ class CreatePrivileges < ActiveRecord::Migration
 
       t.timestamps
     end
-    Privileges.reset_column_information
-    Privileges.create(:privilege_name => 'Customer')
-    Privileges.create(:privilege_name => 'Quotation')
-    Privileges.create(:privilege_name => 'Sales Order')
-    Privileges.create(:privilege_name => 'Invoice')
-    Privileges.create(:privilege_name => 'Customer Cash Register')
-    Privileges.create(:privilege_name => 'New Complaint')
-    Privileges.create(:privilege_name => 'Suppliers')
-    Privileges.create(:privilege_name => 'Purchase Order')
-    Privileges.create(:privilege_name => 'Supplier Cash Register')
-    Privileges.create(:privilege_name => 'Fitters')
-    Privileges.create(:privilege_name => 'Category')
-    Privileges.create(:privilege_name => 'Product')
   end
 
   def self.down
