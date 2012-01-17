c8106dabb1f9ff44917908a639c81e29ff103b4e
Anup Mahabudhe <anupwin@gmail.com>
2012-01-17 18:03:37 +0530

    gem file update

--- config/initializers/carreierwave.rb
+++ config/initializers/carreierwave.rb
@@ -1,10 +1,20 @@
 CarrierWave.configure do |config|
-  config.root = Rails.root.join('tmp') # adding these...
-  config.cache_dir = 'carrierwave' 
+ # config.root = Rails.root.join('tmp') # adding these...
+ # config.cache_dir = 'carrierwave' 
  
   
+  config.fog_credentials = {
+      :provider               => 'AWS',       # required
+      :aws_access_key_id      => 'AKIAIWWQF5REKTC4ZYGQ',       # required
+      :aws_secret_access_key  => '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',       # required
+      :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+    }
+    config.fog_directory  = 'floorsys_demo'                     # required
+   # config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
+   config.fog_public     = true                                   # optional, defaults to true
+   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 
-  config.s3_access_key_id = ENV['AKIAIWWQF5REKTC4ZYGQ']
-  config.s3_secret_access_key = ENV['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']
-  config.s3_bucket = ENV['floorsys_demo']
+
+
+  
 end
