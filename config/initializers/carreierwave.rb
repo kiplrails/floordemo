e6414389e3826c6afd180d21027a6493638a1dc0
Anup Mahabudhe <anupwin@gmail.com>
2012-01-17 18:07:40 +0530

    gem file update

--- config/initializers/carreierwave.rb
+++ config/initializers/carreierwave.rb
@@ -1,6 +1,7 @@
 CarrierWave.configure do |config|
  # config.root = Rails.root.join('tmp') # adding these...
  # config.cache_dir = 'carrierwave' 
+ config.cache_dir = "#{Rails.root}/tmp/uploads"
  
   
   config.fog_credentials = {
